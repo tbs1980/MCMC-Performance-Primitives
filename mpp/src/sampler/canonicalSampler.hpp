244ca3b7a50b7be5cd740f03557dae159e171b98
Sreekumar Thaithara Balan <tbs1980@gmail.com>
2015-07-13 22:39:48 +0100

    docs added for the canonical sampler

--- mpp/src/sampler/canonicalSampler.hpp
+++ mpp/src/sampler/canonicalSampler.hpp
@@ -3,20 +3,76 @@
 
 namespace mpp{ namespace sampler{
 
+    /**
+     * \class canonicalMCMCSampler
+     *
+     * \tparam _engineType MCMC engine type
+     * \tparam _ctrlrType controller type
+     * \tparam _IOType input-output type
+     *
+     * \brief A class for performing MCMC sampling.
+     *
+     * This class performs sampling given the MCMC engine, controller and the IO. The
+     * controller will stop MCMC when the stopping crieterion is satisfied. The IO will
+     * be used to write the chains to the disk.
+     */
     template<class _engineType,class _ctrlrType,class _IOType>
     class canonicalMCMCSampler
     {
     public:
+
+        /**
+         * \typedef _engineType engineType
+         * \brief MCMC engine type
+         */
         typedef _engineType engineType;
+
+        /**
+         * \typedef _ctrlrType ctrlrType
+         * \brief controller type
+         */
         typedef _ctrlrType ctrlrType;
+
+        /**
+         * \typedef _IOType IOType
+         * \brief input-output type
+         */
         typedef _IOType IOType;
+
+        /**
+         * \typedef typename engineType::realMatrixType realMatrixType
+         * \brief defines the real floating point matrix type
+         */
         typedef typename engineType::realMatrixType realMatrixType;
+
+        /**
+         * \typedef typename engineType::realVectorType realVectorType
+         * \brief defines the real floating point vector type
+         */
         typedef typename engineType::realVectorType realVectorType;
+
+        /**
+         * \typedef typename engineType::realScalarType realScalarType
+         * \brief defines the real floating point type
+         */
         typedef typename engineType::realScalarType realScalarType;
 
         static_assert(std::is_floating_point<realScalarType>::value,
             "PARAMETER SHOULD BE A FLOATING POINT TYPE");
 
+        /**
+         * \brief A function that performs the sampling
+         * @param eng   MCMC engine
+         * @param ctrlr controller
+         * @param IO    input-output
+         *
+         * This function calls the generate() function from the MCMC engine repetedly
+         * until the stopping crieterion is statisfied. The random number state and
+         * acceptance rate of the MCMC chain is callled once each iteration using
+         * getRandState() and getAcceptanceRate() functions. The dump() function
+         * is used to pass information to the controller and finally write() function
+         * from IO is used to write chains to the disk. 
+         */
         static void run(engineType & eng,ctrlrType & ctrlr,IOType & IO)
         {
             BOOST_ASSERT_MSG(eng.numParams() == ctrlr.numParams(),
